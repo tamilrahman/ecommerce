c8de8b6c27ccce4a3e1fba0beadcc393c6569ea5
Thamizharasan Jayakumar <tamilrahmanic@gmail.com>
2015-08-11 11:04:00 +0530

    add stock in products

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,5 +1,14 @@
 module ProductsHelper
+	
 	def print_price(price)
 		number_to_currency price
+	end
+
+	def print_stock(stock)
+	  if stock > 0
+	  	content_tag(:p, "In Stock", class: "in-stock")
+	  else
+	  	content_tag(:p, "Sorry! Product is sold out", class: "out-of-stock")
+	  end
 	end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,11 @@
 class Product
   include Mongoid::Document
+  include Mongoid::Timestamps
+
   field :title, type: String
   field :price, type: Float
   field :description, type: String
+  field :stock, type: Integer, default: 0
+
+  validates_presence_of :title, :price, :description
 end
